e6fae4a1ae458d77b94af6f0af37b2210182cdd1
saniyamulla30 <saniyamulla308@gmail.com>
2024-06-27 11:46:32 +0530

    Create RestaurantServiceTest.java

--- RestaurantServiceTest.java
+++ RestaurantServiceTest.java
@@ -5,24 +5,27 @@ import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RestaurantServiceTest {
-    RestaurantService service = new RestaurantService();
+class RestaurantTest {
     Restaurant restaurant;
 
     @BeforeEach
     public void setup() {
-        restaurant = service.addRestaurant("Amelie's cafe", "Chennai", LocalTime.of(9, 0), LocalTime.of(22, 0));
+        restaurant = new Restaurant("Amelie's cafe", "Chennai", LocalTime.of(9, 0), LocalTime.of(22, 0));
         restaurant.addToMenu("Sweet corn soup", 119);
         restaurant.addToMenu("Vegetable lasagne", 269);
     }
 
     @Test
-    public void searching_for_existing_restaurant_should_return_expected_restaurant_object() {
-        assertNotNull(service.findRestaurantByName("Amelie's cafe"));
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time() {
+        Restaurant spyRestaurant = org.mockito.Mockito.spy(restaurant);
+        org.mockito.Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.of(10, 0));
+        assertTrue(spyRestaurant.isRestaurantOpen());
     }
 
     @Test
-    public void searching_for_non_existing_restaurant_should_throw_exception() {
-        assertNull(service.findRestaurantByName("Non-existing cafe"));
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time() {
+        Restaurant spyRestaurant = org.mockito.Mockito.spy(restaurant);
+        org.mockito.Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(LocalTime.of(23, 0));
+        assertFalse(spyRestaurant.isRestaurantOpen());
     }
 }
